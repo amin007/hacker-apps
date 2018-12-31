27aab49ae5a9cc1f7cf733b537be585eb0fc1a74
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-31 13:40:09 +0800

    pindah koding dari semakSiapa() ke dalam dapatPencam

--- Aplikasi/Kelas/Utama/Tanya/masuklah_tanya.php
+++ Aplikasi/Kelas/Utama/Tanya/masuklah_tanya.php
@@ -13,7 +13,25 @@ class Masuklah_Tanya extends \Aplikasi\Kitab\Tanya
 	{
 		echo '<pre>' . $jadual . '=>';
 		print_r($senarai); echo '</pre>';//*/
-		//$this->semakPembolehubah($ujian,'ujian');	}
+		//$this->semakPembolehubah($ujian,'ujian');
+	}
+#------------------------------------------------------------------------------------------#
+	function dapatPencam()
+	{
+		# semak data $_POST
+		list($myTable, $medan01, $medan02, $medan) = dpt_senarai('jadual_login');
+		# semak database
+			$carian[] = array('fix'=>'or(x=)', # cari x= atau %like%
+				'atau'=>'WHERE', # WHERE / OR / AND
+				'medan' => $medan01, # cari dalam medan apa
+				'apa' => $email); # benda yang dicari
+			$carian[] = array('fix'=>'like', # cari x= atau %like%
+				'atau'=>'AND', # WHERE / OR / AND
+				'medan' => $medan02, # cari dalam medan apa
+				'apa' => $password); # benda yang dicari
+
+		return array("`$myTable`", $medan, $carian);
+	}
 #------------------------------------------------------------------------------------------#
 #===========================================================================================
 }
