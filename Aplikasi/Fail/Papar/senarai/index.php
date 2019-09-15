30fa58549723624d17a25bf4996b6360d248bc29
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-09-15 16:25:27 +0800

    wujudkan borang untuk cari user

---
+++ Aplikasi/Fail/Papar/senarai/index.php
@@ -0,0 +1,25 @@
+<?php
+$mencari = URL . 'senarai/carian';
+$butangHantar = 'cari user';
+?>
+<div class="container">
+<form method="POST" action="<?=$mencari?>" class="form-inline" autocomplete="off">
+	<?php //echo $mencari . '<br>' . "\r" ?>
+	<div class="input-group">
+		<input type="text" name="cari" class="form-control rounded" autofocus
+		placeholder="Contoh : user"
+		id="inputString" onkeyup="lookup(this.value);" onblur="fill();">
+		<div class="input-group-append" id="button-addon4">
+			<input type="submit" class="btn btn-outline-secondary rounded" value="<?=$butangHantar?>">
+		</div>
+	</div>
+	<!-- ////////////////////////////////////////////////////////////////////////////////////////// -->
+	<div class="suggestionsBox" id="suggestions" style="display: none; " >
+		<div class="suggestionList" id="autoSuggestionsList">&nbsp;</div>
+	</div>
+</form>
+</div><!-- / class="container" -->
+<br>
+<?php
+# untuk kod baru
+echo '<pre class="bg-secondary text-white">$senarai='; print_r($this->senarai); echo '</pre>';
