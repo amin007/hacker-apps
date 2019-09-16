d51c7b51ad657b9d6d1f662a408f6681c1ecff6c
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-09-16 14:37:36 +0800

    tukar user kepada produk

--- Aplikasi/Fail/Papar/senarai/index.php
+++ Aplikasi/Fail/Papar/senarai/index.php
@@ -1,13 +1,13 @@
 <?php
 $mencari = URL . 'senarai/carian';
-$butangHantar = 'cari user';
+$butangHantar = 'cari produk';
 ?>
 <div class="container">
 <form method="POST" action="<?=$mencari?>" class="form-inline" autocomplete="off">
 	<?php //echo $mencari . '<br>' . "\r" ?>
 	<div class="input-group">
 		<input type="text" name="cari" class="form-control rounded" autofocus
-		placeholder="Contoh : user"
+		placeholder="Contoh : produk"
 		id="inputString" onkeyup="lookup(this.value);" onblur="fill();">
 		<div class="input-group-append" id="button-addon4">
 			<input type="submit" class="btn btn-outline-secondary rounded" value="<?=$butangHantar?>">
