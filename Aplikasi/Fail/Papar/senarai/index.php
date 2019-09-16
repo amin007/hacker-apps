c0fdf227b04c0b45e7d9db6860c169b6c1706c3a
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-09-16 14:39:06 +0800

    tambah contoh jadual

--- Aplikasi/Fail/Papar/senarai/index.php
+++ Aplikasi/Fail/Papar/senarai/index.php
@@ -23,3 +23,56 @@ $butangHantar = 'cari produk';
 <?php
 # untuk kod baru
 echo '<pre class="bg-secondary text-white">$senarai='; print_r($this->senarai); echo '</pre>';
+
+echo "\n" . '<!-- h1> Ini Template Biasa </h1 -->';
+foreach ($this->senarai as $myTable => $row)
+{
+	if ( count($row)==0 ) echo '';
+	else
+	{
+		$tajukjadual = '<span class="badge badge-success">' . $myTable . '</span>'
+		. "\r" . '<span class="badge">' . count($row) . '</span>';
+		//paparTajuk($tajukjadual,$row);
+		pilihJadual($tajukjadual,$row);
+	}# if ( count($row)==0 )
+}
+
+function paparTajuk($tajukjadual, $row)
+{
+#-------------------------------------------------------------------------------------------------
+	echo "\n" . '<h3>' . $tajukjadual . '</h3>';
+#-------------------------------------------------------------------------------------------------
+}
+
+function pilihJadual($tajukjadual, $row)
+{
+#-------------------------------------------------------------------------------------------------
+	echo "\n" . '<!-- Jadual $myTable ########################################### -->';
+	echo "\n" . '<table border="1" class="excel" id="example">';
+	echo "\n" . '<h3>' . $tajukjadual . '</h3>';
+	$printed_headers = false; # mula bina jadual
+	#-----------------------------------------------------------------
+	for ($kira=0; $kira < count($row); $kira++)
+	{
+		if ( !$printed_headers ) # papar tajuk medan sekali sahaja:
+		{
+			echo '<thead><tr><th>#</th>' . "\n";
+			foreach ( array_keys($row[$kira]) as $tajuk )
+			{
+				echo '<th>' . $tajuk . '</th>' . "\n";
+			}
+			echo '</tr></thead>' . "\n";
+			$printed_headers = true;
+		}
+	# papar data $row ------------------------------------------------
+		echo '<tr><td align="center">' . ($kira+1) . '</td>';
+		foreach ( $row[$kira] as $key=>$data )
+		{
+			echo '<td>' . $data . '</td>';
+		}
+		echo '</tr>' . "\n";
+	}#-----------------------------------------------------------------
+	echo '</table>' . "\n"
+	. '<!-- Jadual $myTable ########################################### -->';
+#-------------------------------------------------------------------------------------------------
+}
