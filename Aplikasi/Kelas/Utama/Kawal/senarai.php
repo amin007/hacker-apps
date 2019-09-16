0630725d41a4ab6dbf72d1c75ca3654a61a1f777
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-09-16 14:56:05 +0800

    bina pembolehubah

--- Aplikasi/Kelas/Utama/Kawal/senarai.php
+++ Aplikasi/Kelas/Utama/Kawal/senarai.php
@@ -47,14 +47,15 @@ class Senarai extends \Aplikasi\Kitab\Kawal
 ##------------------------------------------------------------------------------------------------
 	public function index()
 	{
-		echo '<hr> Nama class : ' . __METHOD__ . '<hr>';
-		//$this->paparJadual(); # Set pembolehubah utama
+		//echo '<hr> Nama class : ' . __METHOD__ . '<hr>';
+		# Set pembolehubah utama
+		$this->papar->senarai['produk'] = array();
 
 		# Pergi papar kandungan
 		//$this->_folder = 'cari';
-		//$fail = array('1cari','index','b_ubah');
+		$fail = array('1cari','index','b_ubah');
 		//echo '<br>$fail = ' . $fail[0] . '<hr>';
-		//$this->paparKandungan($this->_folder, $fail[1], $noInclude=0);//*/
+		$this->paparKandungan($this->_folder, $fail[1], $noInclude=0);//*/
 	}
 ##------------------------------------------------------------------------------------------------
 #=================================================================================================
