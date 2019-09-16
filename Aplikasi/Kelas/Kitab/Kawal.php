1d2093196710480c159c751d8731e095b51b9bd4
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-09-16 17:22:44 +0800

    tambah fungsi baru

--- Aplikasi/Kelas/Kitab/Kawal.php
+++ Aplikasi/Kelas/Kitab/Kawal.php
@@ -33,4 +33,28 @@ class Kawal
 
 	}
 #==========================================================================================
+	public function jemaahTaskil02($nama)
+	{
+		$failTanya = GetMatchingFiles(GetContents(TANYA),$nama . '_tanya.php');
+		$tanya = $failTanya[0];
+		/*echo '<br> class Kawal :: $nama : ' . $nama . '|';
+		echo 'TANYA->' . TANYA . '';
+		echo '<pre>$failTanya->'; print_r($failTanya); echo '</pre>';
+		echo '$tanya->' . $tanya . '<br>';
+		//*/
+
+		if (file_exists($tanya))
+		{
+			$tanyaNama = '\\Aplikasi\Tanya\\' . huruf('Besar', $nama) . '_Tanya';
+			//echo '<br>$tanyaNama->' . $tanyaNama . '<br>';
+
+			require_once $tanya;
+			$this->tanya = new $tanyaNama();
+
+			//if (class_exists($tanyaNama)) echo '<br>class ' . $tanyaNama . ' wujud<br>';
+			//else echo '<br>class ' . $tanyaNama . ' tak wujud<br>';
+		}//*/
+
+	}
+#==========================================================================================
 }
