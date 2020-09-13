d67b0e5faabebdf0ff40a8343ab7e723dc1d4563
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-09-13 15:31:07 +0800

    tambah fungsi jenisAlgorithmDaa() dan debugValue($senarai,$jadual,$p='0')

--- sumber/fail/aescode/aes002/test1_process.php
+++ sumber/fail/aescode/aes002/test1_process.php
@@ -75,6 +75,41 @@ function downloadFile($error,$contenuto,$filename)
 	#
 }
 #--------------------------------------------------------------------------------------------------
+function jenisAlgorithmDaa()
+{
+	$ciphers             = openssl_get_cipher_methods();
+	$ciphers_and_aliases = openssl_get_cipher_methods(true);
+	$cipher_aliases      = array_diff($ciphers_and_aliases, $ciphers);
+
+	//ECB mode should be avoided
+	$ciphers = array_filter( $ciphers, function($n) { return stripos($n,"ecb")===FALSE; } );
+
+	//At least as early as Aug 2016, Openssl declared the following weak: RC2, RC4, DES, 3DES, MD5 based
+	$ciphers = array_filter( $ciphers, function($c) { return stripos($c,"des")===FALSE; } );
+	$ciphers = array_filter( $ciphers, function($c) { return stripos($c,"rc2")===FALSE; } );
+	$ciphers = array_filter( $ciphers, function($c) { return stripos($c,"rc4")===FALSE; } );
+	$ciphers = array_filter( $ciphers, function($c) { return stripos($c,"md5")===FALSE; } );
+	$cipher_aliases = array_filter($cipher_aliases,function($c) { return stripos($c,"des")===FALSE; } );
+	$cipher_aliases = array_filter($cipher_aliases,function($c) { return stripos($c,"rc2")===FALSE; } );
+
+	//print_r($ciphers);
+	//print_r($cipher_aliases);
+	#
+	return array($ciphers,$cipher_aliases);
+}
+#--------------------------------------------------------------------------------------------------
+	function debugValue($senarai,$jadual,$p='0')
+	{
+		echo '<pre>$' . $jadual . '=><br>';
+		if($p == '0') print_r($senarai);
+		if($p == '1') var_export($senarai);
+		echo '</pre><hr>';//*/
+		//debugValue($ujian,'ujian',0);
+		//$this->debugValue($ujian,'ujian',0);
+		#http://php.net/manual/en/function.var-export.php
+		#http://php.net/manual/en/function.print-r.php
+	}
+#--------------------------------------------------------------------------------------------------
 //$ALGORITHM = 'AES-128-CBC';
 //$IV    = '12dasdq3g5b2434b';
 define('ALGORITHM', 'AES-128-CBC');
